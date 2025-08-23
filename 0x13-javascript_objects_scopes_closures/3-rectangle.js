cee8dc2c2522f80ddc904a4e53d11ae0373a9742
Tobore1 <ogbodutobore@gmail.com>
2025-08-23 03:19:00 -0700

    ans1

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,26 +1,10 @@
-class Rectangle {
-  /**
-   * Creates an instance of Rectangle.
-   * If w or h is not a positive integer, it creates an empty object.
-   * @param {number} w - The width of the rectangle.
-   * @param {number} h - The height of the rectangle.
-   */
-  constructor(w, h) {
-    // Check if w or h are not positive integers
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
-  /**
-   * Prints the rectangle using the character 'X'.
-   */
-  print() {
-    if (this.width && this.height) {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
-    }
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
-}
+};
